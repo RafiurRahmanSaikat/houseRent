f50f67cf7596b06668b990d93d3f14ed427e1853
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2024-10-26 20:35:32 +0600

    Settings Chnged

--- rent_ease/settings.py
+++ rent_ease/settings.py
@@ -32,9 +32,12 @@ ALLOWED_HOSTS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://house-rent-lyart.vercel.app",
+    "https://house-rent-ease.netlify.app",
+    "https://house-rent-backend.onrender.com",
     "http://127.0.0.1",
     "http://localhost:3000",
-    "http://localhost:5173",
     "https://house-rent-ease.netlify.app",
     "https://house-rent-backend.onrender.com",
     "https://house-rent-lyart.vercel.app",
@@ -81,6 +84,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -88,7 +92,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "rent_ease.urls"
